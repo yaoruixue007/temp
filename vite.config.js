b878b68c768137d57ab2847d299918ebbad62cb1
fengxuefeng <974692764@qq.com>
2023-06-16 15:44:06 +0800

    '打包路径调整'

--- vite.config.js
+++ vite.config.js
@@ -68,7 +68,7 @@ export default () => defineConfig({
   //浏览器兼容性  "esnext"|"modules"
   target: "modules",
   //指定输出路径
-  outDir: "dist",
+  outDir: "./dist/ui",
   //生成静态资源的存放路径
   assetsDir: "assets",
   //小于此阈值的导入或引用资源将内联为 base64 编码，以避免额外的 http 请求。设置为 0 可以完全禁用此项
