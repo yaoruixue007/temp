717e998e53604eeb0e45f7c0a017afa8400cc7ea
fengxuefeng <974692764@qq.com>
2023-07-27 16:22:57 +0800

    '修改base路径'

--- vite.config.js
+++ vite.config.js
@@ -17,7 +17,7 @@ export default () => defineConfig({
 
  //静态资源服务的文件夹
  publicDir: "public",
- base: '/',
+ base: './',
  //静态资源处理
  assetsInclude: "",
  //控制台输出的级别 info 、warn、error、silent
@@ -68,7 +68,7 @@ export default () => defineConfig({
   //浏览器兼容性  "esnext"|"modules"
   target: "modules",
   //指定输出路径
-  outDir: "./dist/ui",
+  outDir: "./dist",
   //生成静态资源的存放路径
   assetsDir: "assets",
   //小于此阈值的导入或引用资源将内联为 base64 编码，以避免额外的 http 请求。设置为 0 可以完全禁用此项
